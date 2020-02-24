85395daf1a11cbd45c171c9f8a5ed6b01927ac9a
Virgil <virgil031299@gmail.com>
2020-02-24 11:39:24 +0100

    Les deux bouton crypt et decrypt ouvrent un selecteur de fichier

--- src/view/FileView.java
+++ src/view/FileView.java
@@ -1,5 +1,10 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
@@ -10,31 +15,49 @@ public class FileView {
 	private Stage secondaryStage;
 	private Scene secondaryScene;
 	private VBox fileView;
-	
+	private TextArea textArea;
+
 	public FileView() {
-		
+
 	}
-	
+
 	public Stage createFileView(Stage primaryStage) {
 		this.secondaryStage = new Stage();
-		
-		this.secondaryScene = createSceneSecondaryScene();
-		
+
 		this.secondaryStage.setScene(this.secondaryScene);
 		this.secondaryStage.centerOnScreen();
 		this.secondaryStage.initModality(Modality.WINDOW_MODAL);
 		this.secondaryStage.initOwner(primaryStage);
 		this.secondaryStage.show();
-		
+
 		return this.secondaryStage;
 	}
 
-	private Scene createSceneSecondaryScene() {
+	public Scene createSceneSecondaryScene(File selFile) {
 		this.fileView = new VBox();
-		this.fileView.getChildren().add(new TextArea("NOT IMPLEMENTED YES"));
+		this.textArea = new TextArea();
+
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(selFile));
+			String str;
+			while ((str = in.readLine()) != null) {
+				System.out.println(str);
+				this.textArea.appendText(str);
+			}
+		} catch (IOException e) {
+		} finally {
+			try { 
+				in.close(); 
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		this.fileView.getChildren().add(this.textArea);
 		return new Scene(this.fileView);
 	}
-	
+
 	public Scene getSecondaryScene() {
 		return this.secondaryScene;
 	}
--- src/view/MainView.java
+++ src/view/MainView.java
@@ -4,30 +4,35 @@ import java.awt.Desktop;
 import java.awt.GraphicsEnvironment;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class MainView {
 	private Stage primaryStage;
+	private Stage secondaryStage;
 	private HBox mainView;
 	private Scene sc;
-	
+
 	private DecryptView decryptView;
 	private CryptView cryptView;
 	private FileView fileView;
-	
+
 	private static GraphicsEnvironment ge;
-	
+
 	public MainView(Stage primaryStage) {
 		ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		
+
 		this.mainView = new HBox();
 		this.decryptView = new DecryptView();
 		this.cryptView = new CryptView();
 		this.fileView = new FileView();
-		
+
 		this.primaryStage = primaryStage;
 	}
 
@@ -42,7 +47,7 @@ public class MainView {
 
 		this.mainView.getChildren().addAll(this.cryptView.getCryptView(), this.decryptView.getDecryptView());
 		this.setOnActionMainView();
-		
+
 		this.sc = new Scene(this.mainView);
 		this.primaryStage.setScene(this.sc);
 		this.primaryStage.show();
@@ -50,15 +55,20 @@ public class MainView {
 
 	private void setOnActionMainView() {
 		this.decryptView.getSubmitDecryptButton().setOnAction(e -> {
-			this.fileView.createFileView(this.primaryStage);
+			File fileToDecrypt = this.getFileByOpenDialog(this.getExtensionAccepted());
+			this.secondaryStage = this.fileView.createFileView(this.primaryStage);
+			this.fileView.createSceneSecondaryScene(fileToDecrypt);
 		});
-		this.decryptView.getSubmitDecryptButton().setOnAction(e -> {
-			File file = new File ("c:");
-			Desktop desktop = Desktop.getDesktop();
+		this.cryptView.getSumbmitCryptButton().setOnAction(e -> {
+			File fileToDecrypt = this.getFileByOpenDialog(this.getExtensionAccepted());
+
+			/**
+			 * TRAITEMENT DU FICHIER fileToDecrypt
+			 */
+
 			try {
-				desktop.open(file);
+				Desktop.getDesktop().open(fileToDecrypt);
 			} catch (IOException e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 		});
@@ -70,5 +80,27 @@ public class MainView {
 
 	public static double getWinWidth() {
 		return ge.getMaximumWindowBounds().getWidth();
+	}
+
+	public File getFileByOpenDialog(List<ExtensionFilter> filter) {
+		FileChooser fileChooser = new FileChooser();
+
+		fileChooser.getExtensionFilters().addAll(filter);
+		fileChooser.setTitle("Open Resource File");
+
+		File fileChoosen = fileChooser.showOpenDialog(primaryStage);
+		System.out.println(fileChoosen.getName());
+
+		return fileChoosen;
+	}
+
+	public List<ExtensionFilter> getExtensionAccepted() {
+		List<ExtensionFilter> extensionAccepted = new ArrayList<ExtensionFilter> ();
+
+		extensionAccepted.add(new FileChooser.ExtensionFilter("JPG", "*.jpg"));
+		extensionAccepted.add(new FileChooser.ExtensionFilter("PNG", "*.png"));
+		extensionAccepted.add(new FileChooser.ExtensionFilter("TXT", "*.txt"));
+
+		return extensionAccepted;
 	}
 }
